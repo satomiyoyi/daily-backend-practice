0417f6a6047561740ca767e3f57c44a74c9ac8a7
Lin Xu <linxu1@microsoft.com>
2026-06-04 16:18:14 +0800

    fix day2 review bugs

--- week-01-http-express/day-02/solution/index.ts
+++ week-01-http-express/day-02/solution/index.ts
@@ -3,27 +3,31 @@ import express from 'express';
 const app = express();
 
 const todos = [
-  { id: 1, title: 'Learn TypeScript', completed: false },
-  { id: 2, title: 'Build a REST API', completed: false },
-  { id: 3, title: 'Write unit tests', completed: false },
+    { id: 1, title: 'Learn TypeScript', done: false, createAt: new Date() },
+    { id: 2, title: 'Build a REST API', done: false, createAt: new Date() },
+    { id: 3, title: 'Write unit tests', done: false, createAt: new Date() },
 ];
 app.get('/todos', (req, res) => {
-  res.json(todos);
+    res.json(todos);
 });
 
 app.get('/todos/:id', (req, res) => {
-  const { id } = req.params;
-  const todo = todos.find(todo => todo.id === parseInt(id));
-  if (todo) {
-    res.json(todo);
-  } else {
-    res.status(404).json({ message: 'Todo not found' });
-  }
+    const { id } = req.params;
+    const todo = todos.find(todo => todo.id === parseInt(id));
+    if (todo) {
+        res.json(todo);
+    } else {
+        res.status(404).json({ message: 'Todo not found' });
+    }
 });
 
+app.use(express.json());
 app.post('/todos', (req, res) => {
     const { title } = req.body;
-    const newTodo = { id: Date.now(), title, completed: false };
+    if (!title || typeof title !== 'string') {
+        return res.status(400).json({ message: 'Title is required and must be a string' });
+    }
+    const newTodo = { id: todos.length + 1, title, done: false, createAt: new Date() };
     todos.push(newTodo);
     res.status(201).json(newTodo);
 });
@@ -40,5 +44,5 @@ app.delete('/todos/:id', (req, res) => {
 });
 
 app.listen(3003, () => {
-  console.log('Server is running on http://localhost:3003');
+    console.log('Server is running on http://localhost:3003');
 });
--- week-01-http-express/day-02/solution/learn.md
+++ week-01-http-express/day-02/solution/learn.md
@@ -7,3 +7,8 @@
 /todo/：id 更向资源；优点更清晰；确定可能需要额外社会子路由
 /todo/getId?id=123 更倾向查找；有点拓展性好不断叠加参数；缺点不太符合rest设计风格
 
+resful优势： 把api当作资源的集合来操作，动作由http method来表达，统一接口，可缓存，url本身有语义。
+rpc优势： 复杂查询更直观
+
+测试post接口语句
+Invoke-WebRequest -Uri "http://localhost:3003/todos" -Method Post -ContentType "application/json" -Body '{"title":"htitle4"}' | Select-Object StatusCode, Content
